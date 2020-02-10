a7d4132635564c98bcff23aa3dd11c3851567f3f
vibraniumforge <gregm9999@gmail.com>
2020-02-10 12:41:39 -0800

    added welcome flash

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
   def home
     if session[:user_id]
       @user = User.find(session[:user_id])
+      flash[:success] = "Welcome!"
     end
   end
 
