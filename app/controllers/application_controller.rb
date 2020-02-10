9ff4f2d65dfe5f4727faf71a99ee47a69e00a0ed
vibraniumforge <gregm9999@gmail.com>
2020-02-10 12:40:22 -0800

    refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
     def current_user
         if session[:user_id].present?
-            user = User.find_by(:id => session[:user_id])
+            @user = User.find(session[:user_id])
         end
     end
 
