b8163978b7037fabdc2a50bce85e053c913b2909
vibraniumforge <gregm9999@gmail.com>
2020-02-09 19:00:14 -0800

    removed f.key

--- db/migrate/20190112000207_create_superpowers.rb
+++ db/migrate/20190112000207_create_superpowers.rb
@@ -2,7 +2,6 @@ class CreateSuperpowers < ActiveRecord::Migration[5.2]
   def change
     create_table :superpowers do |t|
       t.string :name
-      t.integer :character_id
 
       t.timestamps
     end
