23e5727edf27570bbdfc5666d402ba0260d0de3f
vibraniumforge <gregm9999@gmail.com>
2020-02-16 17:02:25 -0800

    refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,7 @@ class ApplicationController < ActionController::Base
     helper_method :current_user, :require_login
 
     def current_user
-        if session[:user_id].present?
-            @user = User.find(session[:user_id])
-        end
+        @current_user ||= User.find(session[:user_id]) if session[:user_id] 
     end
 
     def require_login
